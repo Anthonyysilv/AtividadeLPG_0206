82fdbd98a53db1f04b4204f0566b04d968e2c1b2
Anthony Silva <110927322+Anthonyysilv@users.noreply.github.com>
2023-06-02 12:13:57 -0300

    Update Program.cs

--- AtividadeLPG_0206/Program.cs
+++ AtividadeLPG_0206/Program.cs
@@ -6,6 +6,14 @@
 //Recuperação 6 > média >= 3
 //Reprovado média < 3
 using System;
+
+interface IOperacao
+{
+    void CalcularMedia(Aluno aluno, double nota1, double nota2);
+    void CalcularMedia(Aluno aluno, double nota1, double nota2, double nota3, double nota4);
+    void ExibirRelatorio(Aluno[] alunos);
+}
+
 class Aluno
 {
     public string nomeAluno;
@@ -13,40 +21,138 @@ class Aluno
     public int serieAluno;
     public string cursoAluno;
 
-    public Aluno(string curso)
+    public Aluno(string nome, int codigo, int serie, string curso)
     {
-        
+        nomeAluno = nome;
+        codigoAluno = codigo;
+        serieAluno = serie;
+        cursoAluno = curso;
     }
 }
-class MediaFinal
+
+class MediaFinal : IOperacao
 {
-    public double nota1;
-    public double nota2;
-    public double nota3;
-    public double nota4;
-    public MediaFinal(double mediaF)
+    public void CalcularMedia(Aluno aluno, double nota1, double nota2)
     {
-        mediaF = (this.nota1 + this.nota2 + this.nota3 + this.nota4) / 4;
+        double mediaSemestral = (nota1 + nota2) / 2;
+        Console.WriteLine($"Média semestral do aluno {aluno.nomeAluno}: {mediaSemestral}");
+        VerificarStatus(aluno, mediaSemestral);
+    }
+
+    public void CalcularMedia(Aluno aluno, double nota1, double nota2, double nota3, double nota4)
+    {
+        double mediaFinal = (nota1 + nota2 + nota3 + nota4) / 4;
+        Console.WriteLine($"Média final do aluno {aluno.nomeAluno}: {mediaFinal}");
+        VerificarStatus(aluno, mediaFinal);
+    }
+
+    public void ExibirRelatorio(Aluno[] alunos)
+    {
+        Console.WriteLine("Relatório dos alunos:");
+        foreach (var aluno in alunos)
+        {
+            Console.WriteLine($"Nome: {aluno.nomeAluno}, Código: {aluno.codigoAluno}, Série: {aluno.serieAluno}, Curso: {aluno.cursoAluno}");
+        }
+    }
+
+    private void VerificarStatus(Aluno aluno, double media)
+    {
+        if (media < 3)
+        {
+            Console.WriteLine($"Situação do aluno {aluno.nomeAluno}: Reprovado");
+        }
+        else if (media < 6)
+        {
+            Console.WriteLine($"Situação do aluno {aluno.nomeAluno}: Recuperação");
+        }
+        else
+        {
+            Console.WriteLine($"Situação do aluno {aluno.nomeAluno}: Aprovado");
+        }
     }
 }
+
 class Program
 {
     static void Main(string[] args)
     {
-        int opc, opc2;
-        Console.WriteLine("------------------Sistema Escolar------------------");
-        Console.WriteLine("Informe, dentre as opções, o'que se deseja fazer");
-        Console.WriteLine("1 - Acessar informações de um aluno");
-        Console.WriteLine("2 - Adicionar notas para o calculo da média final");
-        Console.WriteLine("3 - Adicionar notas para o calculo da média semestral");
-        Console.WriteLine("4 - Filtrar alunos aprovados/recuperação/reprovados");
-        Console.WriteLine("5 - Relatório dos alunos");
-        opc = Convert.ToInt32(Console.ReadLine());
+        Aluno[] alunos = new Aluno[5];
+        alunos[0] = new Aluno("João", 1, 8, "Matemática");
+        alunos[1] = new Aluno("Maria", 2, 9, "Ciências");
+        alunos[2] = new Aluno("Pedro", 3, 7, "História");
+        alunos[3] = new Aluno("Ana", 4, 8, "Geografia");
+        alunos[4] = new Aluno("Lucas", 5, 9, "Português");
+
+        MediaFinal mediaFinal = new MediaFinal();
+
+        Console.WriteLine("Selecione a operação:");
+        Console.WriteLine("1 - Calcular média semestral");
+        Console.WriteLine("2 - Calcular média final");
+        Console.WriteLine("3 - Exibir relatório dos alunos");
+
+        int opc = Convert.ToInt32(Console.ReadLine());
+
         switch (opc)
         {
             case 1:
-                Console.WriteLine("Informe o nome");
+                Console.WriteLine("Informe o código do aluno:");
+                int codigoAlunoSemestral = Convert.ToInt32(Console.ReadLine());
+                Aluno alunoSemestral = BuscarAlunoPorCodigo(alunos, codigoAlunoSemestral);
+                if (alunoSemestral != null)
+                {
+                    Console.WriteLine("Informe a nota 1:");
+                    double nota1Semestral = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Informe a nota 2:");
+                    double nota2Semestral = Convert.ToDouble(Console.ReadLine());
+                    mediaFinal.CalcularMedia(alunoSemestral, nota1Semestral, nota2Semestral);
+                }
+                else
+                {
+                    Console.WriteLine("Aluno não encontrado.");
+                }
                 break;
-        }    
+
+            case 2:
+                Console.WriteLine("Informe o código do aluno:");
+                int codigoAlunoFinal = Convert.ToInt32(Console.ReadLine());
+                Aluno alunoFinal = BuscarAlunoPorCodigo(alunos, codigoAlunoFinal);
+                if (alunoFinal != null)
+                {
+                    Console.WriteLine("Informe a nota 1:");
+                    double nota1Final = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Informe a nota 2:");
+                    double nota2Final = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Informe a nota 3:");
+                    double nota3Final = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Informe a nota 4:");
+                    double nota4Final = Convert.ToDouble(Console.ReadLine());
+                    mediaFinal.CalcularMedia(alunoFinal, nota1Final, nota2Final, nota3Final, nota4Final);
+                }
+                else
+                {
+                    Console.WriteLine("Aluno não encontrado.");
+                }
+                break;
+
+            case 3:
+                mediaFinal.ExibirRelatorio(alunos);
+                break;
+
+            default:
+                Console.WriteLine("Opção inválida.");
+                break;
+        }
+    }
+
+    static Aluno BuscarAlunoPorCodigo(Aluno[] alunos, int codigo)
+    {
+        foreach (var aluno in alunos)
+        {
+            if (aluno.codigoAluno == codigo)
+            {
+                return aluno;
+            }
+        }
+        return null;
     }
 }
